a4f3fff61617fa06a8da1217fac91d83939a1a1e
Ed Brook <edbrook@gmail.com>
2017-09-30 19:43:41 +0100

    Initial import

---
+++ app/src/main/java/uk/co/dekoorb/android/bitcoinvalue/BitcoinValueActivity.java
@@ -0,0 +1,13 @@
+package uk.co.dekoorb.android.bitcoinvalue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BitcoinValueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bitcoin_value);
+    }
+}
